e2301144f9dc7c28a92bfbcb18b739682d490742
Jeff Purvis <jeffp@zoeller.com>
2025-04-10 11:03:58 -0400

    final app

--- src/H1.jsx
+++
@@ -1,9 +0,0 @@
-const H1 = () => {
-  return (
-    <h1 className="first-heading">
-      Word<span className="first-heading--thin">Analytics</span>
-    </h1>
-  );
-};
-
-export default H1;
--- src/Stats.jsx
+++
@@ -1,5 +0,0 @@
-const Stats = () => {
-  return <section className="stats">stats</section>;
-};
-
-export default Stats;
